cf3d59d1b804cccfd1878e7575ef513da0fcb4de
Andrew Nesbitt <andrewnez@gmail.com>
2016-07-04 23:46:38 +0100

    first commit

---
+++ lib/dotyaml.rb
@@ -0,0 +1,82 @@
+require "dotyaml/version"
+require 'active_support/core_ext/hash/indifferent_access'
+
+module Dotyaml
+  class Test
+    attr_accessor :manifests
+    attr_accessor :config
+
+    def initialize(manifests, config)
+      @manifests = manifests
+      @config = HashWithIndifferentAccess.new(config)
+      p @config
+    end
+
+    def setup
+      manifests.map do |manifest|
+        map_manifest(manifest)
+      end
+    end
+
+    def map_manifest(manifest)
+      {
+        :platform => manifest[:platform],
+        :path => manifest[:path],
+        :dependencies => manifest[:dependencies].map do |dependency|
+          map_dependency(manifest, dependency)
+        end
+      }
+    end
+
+    def map_dependency(manifest, dependency)
+      {
+        :name => dependency[:name],
+        :requirement => dependency[:requirement],
+        :type => dependency[:type],
+        :tests => tests.map do |test_name, default_value|
+          map_test(manifest, dependency, test_name, default_value)
+        end.to_h
+      }
+    end
+
+    def map_test(manifest, dependency, test_name, default_value)
+      should_run = default_value
+
+      # test name
+      test_name_config = config.fetch('tests', {}).fetch(test_name, nil)
+      should_run = test_name_config if test_name_config
+
+      # type
+      type_config = config.fetch('type', {}).fetch(dependency[:type], {}).fetch('tests', {}).fetch(test_name, nil)
+      should_run = type_config if type_config
+
+      # filename
+      filename_config = config.fetch(remove_tmp_path(manifest[:path]), {}).fetch('tests', {}).fetch(test_name, nil)
+      should_run = filename_config if filename_config
+
+      # platform
+      platform_config = config.fetch(manifest[:platform], {}).fetch('tests', {}).fetch(test_name, nil)
+      should_run = platform_config if platform_config
+
+      # project name
+      project_config = config.fetch(manifest[:platform], {}).fetch(dependency[:name], {}).fetch('tests', {}).fetch(test_name, nil)
+      should_run = project_config if project_config
+
+      [test_name, should_run.to_s]
+    end
+
+    def remove_tmp_path(manifest_path)
+      manifest_path.match(/tmp\/\d+\/(.+)/)[1]
+    end
+
+    def tests
+      {
+        :removed      => "fail",
+        :deprecated   => "fail",
+        :unmaintained => "fail",
+        :unlicensed   => "fail",
+        :outdated     => "warn"
+      }
+    end
+  end
+end
---
+++ spec/dotyaml_spec.rb
@@ -0,0 +1,135 @@
+require 'spec_helper'
+
+describe Dotyaml do
+  let(:manifests) { [
+    {
+      :platform=>"Rubygems",
+      :path=>"tmp/125/Gemfile",
+      :dependencies=>[
+        {
+          :name=>"rails",
+          :requirement=>"= 4.2.6",
+          :type=>:runtime
+        }
+      ]
+    }
+  ] }
+
+  it 'has a version number' do
+    expect(Dotyaml::VERSION).not_to be nil
+  end
+
+  it 'runs tests with empty config' do
+    config = {}
+    tester = Dotyaml::Test.new(manifests, config)
+    expect(tester.setup).to eq([
+       {
+         :platform=>"Rubygems",
+         :path=>"tmp/125/Gemfile",
+         :dependencies=>
+          [{:name=>"rails",
+            :requirement=>"= 4.2.6",
+            :type=>:runtime,
+            :tests=>
+             {:removed=>"fail",
+              :deprecated=>"fail",
+              :unmaintained=>"fail",
+              :unlicensed=>"fail",
+              :outdated=>"warn"}}]}])
+  end
+
+  it 'skips all of one kind of test' do
+    config = {"tests"=>{"removed"=>'skip'}}
+    tester = Dotyaml::Test.new(manifests, config)
+    expect(tester.setup).to eq([
+       {
+         :platform=>"Rubygems",
+         :path=>"tmp/125/Gemfile",
+         :dependencies=>
+          [{:name=>"rails",
+            :requirement=>"= 4.2.6",
+            :type=>:runtime,
+            :tests=>
+             {:removed=>"skip",
+              :deprecated=>"fail",
+              :unmaintained=>"fail",
+              :unlicensed=>"fail",
+              :outdated=>"warn"}}]}])
+  end
+
+  it 'skips all of one kind of test for a file' do
+    config = {"Gemfile"=>{"tests"=>{"removed"=>:skip}}}
+    tester = Dotyaml::Test.new(manifests, config)
+    expect(tester.setup).to eq([
+       {
+         :platform=>"Rubygems",
+         :path=>"tmp/125/Gemfile",
+         :dependencies=>
+          [{:name=>"rails",
+            :requirement=>"= 4.2.6",
+            :type=>:runtime,
+            :tests=>
+             {:removed=>"skip",
+              :deprecated=>"fail",
+              :unmaintained=>"fail",
+              :unlicensed=>"fail",
+              :outdated=>"warn"}}]}])
+  end
+
+  it 'skips all of one kind of test for a platform' do
+    config = {"Rubygems"=>{"tests"=>{"removed"=>:skip}}}
+    tester = Dotyaml::Test.new(manifests, config)
+    expect(tester.setup).to eq([
+       {
+         :platform=>"Rubygems",
+         :path=>"tmp/125/Gemfile",
+         :dependencies=>
+          [{:name=>"rails",
+            :requirement=>"= 4.2.6",
+            :type=>:runtime,
+            :tests=>
+             {:removed=>"skip",
+              :deprecated=>"fail",
+              :unmaintained=>"fail",
+              :unlicensed=>"fail",
+              :outdated=>"warn"}}]}])
+  end
+
+  it 'skips all of one kind of test for a project' do
+    config = {"Rubygems"=>{"rails" => {"tests"=>{"removed"=>:skip}}}}
+    tester = Dotyaml::Test.new(manifests, config)
+    expect(tester.setup).to eq([
+       {
+         :platform=>"Rubygems",
+         :path=>"tmp/125/Gemfile",
+         :dependencies=>
+          [{:name=>"rails",
+            :requirement=>"= 4.2.6",
+            :type=>:runtime,
+            :tests=>
+             {:removed=>"skip",
+              :deprecated=>"fail",
+              :unmaintained=>"fail",
+              :unlicensed=>"fail",
+              :outdated=>"warn"}}]}])
+  end
+
+  it 'skips all of one kind of test for a runtime' do
+    config = {"type"=>{"runtime" => {"tests"=>{"removed"=>:skip}}}}
+    tester = Dotyaml::Test.new(manifests, config)
+    expect(tester.setup).to eq([
+       {
+         :platform=>"Rubygems",
+         :path=>"tmp/125/Gemfile",
+         :dependencies=>
+          [{:name=>"rails",
+            :requirement=>"= 4.2.6",
+            :type=>:runtime,
+            :tests=>
+             {:removed=>"skip",
+              :deprecated=>"fail",
+              :unmaintained=>"fail",
+              :unlicensed=>"fail",
+              :outdated=>"warn"}}]}])
+  end
+end
